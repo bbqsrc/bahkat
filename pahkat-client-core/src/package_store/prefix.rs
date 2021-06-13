c5bd813e0d47389d80a6f661a9a4c91f5fa4f571
Brendan Molloy <brendan@technocreatives.com>
2021-06-13 13:50:02 +0200

    Fix unnecessary sync

--- pahkat-client-core/src/package_store/prefix.rs
+++ pahkat-client-core/src/package_store/prefix.rs
@@ -199,7 +199,6 @@ impl PackageStore for PrefixPackageStore {
         Box<
             dyn futures::stream::Stream<Item = crate::package_store::DownloadEvent>
                 + Send
-                + Sync
                 + 'static,
         >,
     > {
